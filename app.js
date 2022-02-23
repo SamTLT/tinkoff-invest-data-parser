fe88d02aad204a9fe76bf0d07dbb905d17cdd51c
Iakov Khovalkin <iakov.khovalkin@sirenltd.com>
2022-02-23 15:13:03 +0300

    Add error handling

--- app.js
+++ app.js
@@ -15,9 +15,16 @@ const server = http.createServer(async (req, res) => {
       res.write('Missing Authorization Header');
       res.end();
     } else {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify(await data.getData()));
-      res.end();
+      data.getData().then(data => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(data));
+        res.end();
+      }).catch((err) => {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(err));
+        res.end();
+      });
+
     }
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
