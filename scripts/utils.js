b15e705cfb3c49b96e342930d58b4a366577f07d
Iakov Khovalkin <iakov.khovalkin@sirenltd.com>
2022-02-23 15:35:58 +0300

    Cache update

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,31 +1,55 @@
 const getLastUpdateString = () => {
   const date = new Date();
-  return `${date.toLocaleDateString('ru-RU')} ${date.toLocaleTimeString('ru-RU')}`;
+  return `${date.toLocaleDateString('ru-RU')} ${date.toLocaleTimeString(
+    'ru-RU'
+  )}`;
 };
 
-let cache;
-let isCached = false;
+const awaitTimeout = (ms) => {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve('done!');
+    }, ms);
+  });
+};
+
+let cacheData;
+let isCacheFresh = false;
+let isLoading = false;
 
 const addCache = async (fn, ms) => {
-  if (!isCached) {
-    try {
-      const data = await fn();
+  if (!isCacheFresh) {
+    if (!isLoading) {
+      try {
+        isLoading = true;
+        const data = await fn();
 
-      isCached = true;
-      cache = data;
-      setTimeout(() => (isCached = false), ms);
+        cacheData = data;
+        isCacheFresh = true;
+        setTimeout(() => (isCacheFresh = false), ms);
+        isLoading = false;
 
-      return data;
-    } catch (err) {
-      console.error('addCache: Failed to execute passed function');
-      console.error(err);
-      isCached = false;
+        return data;
+      } catch (err) {
+        console.error('addCache: Failed to execute passed function');
+        console.error(err);
 
-      throw new Error('addCache: Failed to execute passed function');
+        if (cacheData) {
+          return cacheData;
+        }
+
+        throw new Error('addCache: No cache to return');
+      }
+    } else {
+      await awaitTimeout(ms);
+
+      if (!cacheData) {
+        throw new Error('addCache: No cache to return');
+      }
     }
   }
 
-  return cache;
+  return cacheData;
 };
 
 module.exports = { getLastUpdateString, addCache };
