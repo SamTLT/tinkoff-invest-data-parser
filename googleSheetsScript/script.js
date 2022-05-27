b68f7d3d89e7610f44346d995090cffdd99ad3a9
Iakov Khovalkin <iakov.khovalkin@sirenltd.com>
2022-05-27 20:34:10 +0400

    fix getTinkoffSnp500Data function

--- googleSheetsScript/script.js
+++ googleSheetsScript/script.js
@@ -1,5 +1,5 @@
 const STOCKS_JSON = 'https://service.endpoint/api';
-const TINKOFF_FUNDS_DATA = 'https://service.endpoint/api';
+const TINKOFF_FUNDS_SNP500_DATA = STOCKS_JSON + '/tinkoff-funds?name=%D0%A2%D0%B8%D0%BD%D1%8C%D0%BA%D0%BE%D1%84%D1%84%20S%26P%20500&column=%D0%9F%D0%B0%D0%B9%20%D0%B7%D0%B0%20%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BD%D0%B8%D0%B9%20%D0%B4%D0%B5%D0%BD%D1%8C';
 
 function httpGetJSON(url) {
   const response = UrlFetchApp.fetch(url);
@@ -67,10 +67,12 @@ function getTickerExpectedYield(ticker) {
   return 0;
 }
 
-function getTinkoffFundData(name, column) {
-  if (name || column) {
-    return httpGetJSON(TINKOFF_FUNDS_DATA + '?=name' + name + '&column=' + column);
+function getTinkoffSnP500Data() {
+  const JSON = httpGetJSON(TINKOFF_FUNDS_SNP500_DATA);
+
+  if (!JSON.message) {
+    return 'error'; 
   }
 
-  return 'Please pass name and column';
+  return JSON.message.toString().replace(/\./g, ',');
 }
